7f2fc5557c96c31aafe020dfa21df96251957718
davidkablee <sirdavidudoji07@gmail.com>
2025-04-25 00:21:45 +0100

    Retrying

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import User from './src/models/User.js';
 import Report from './src/models/Report.js';
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 // Connect to MongoDB
 connectDB();
